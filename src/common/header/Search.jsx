6e391fec844e18fe602bedb18246cce4b968b463
Ahmed Maher Algohary <ahmedmaheraljwhry057@gmail.com>
2024-09-18 21:51:29 +0300

    Modified the visibility of the login menu

--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "../../components/assets/images/logo1.svg";
 import { Link } from "react-router-dom";
 
 const Search = ({ cartItem }) => {
 	const [showLogin, setShowLogin] = useState(false)
+	const loginRef = useRef(null); // Reference to the login dropdown
+
 	// to toggle 'active' class based on scroll position
 	window.addEventListener("scroll", function () {
 		const search = document.querySelector(".search");
 		search.classList.toggle("active", window.scrollY > 100);
 	});
+
+	// Handle clicks outside the login dropdown
+	useEffect(() => {
+		const handleClickOutside = (event) => {
+		  if (loginRef.current && !loginRef.current.contains(event.target)) {
+			setShowLogin(false); // Close the dropdown if clicked outside
+		  }
+		};
+		// Add event listener
+		document.addEventListener("mousedown", handleClickOutside);
+		// Remove event listener on cleanup
+		return () => {
+		  document.removeEventListener("mousedown", handleClickOutside);
+		};
+	  }, [loginRef]);
+
 	return (
 		<section className="search">
 			<div className="container c_flex">
@@ -30,13 +48,13 @@ const Search = ({ cartItem }) => {
 
 				{/* User Icon and Cart Section */}
 				<div className="icon f_flex width">
-					<div className="icon-Circle">
+					<div className="icon-Circle" ref={loginRef}>
 					<i className="fa fa-user icon-circle" onClick={() => setShowLogin(prev => !prev)}></i>
 					<ul style={{display: `${showLogin? 'block' : 'none'}`}}>
-						<li>
+						<li onClick={() => setShowLogin(false)}>
 							<Link to='/login'>Login</Link>
 						</li>
-						<li>
+						<li onClick={() => setShowLogin(false)}>
 							<Link to='/register'>Register</Link>
 						</li>
 					</ul>
